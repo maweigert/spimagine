550ce58d9c7067b11702bc656be97e7aa1bee166
Martin Weigert <mweigert@mpi-cbg.de>
2014-04-23 10:59:50 +0200

    setup fixed
    Former-commit-id: 16ddcd87f860ed07cdf6aeaf70598a1979f85e2b

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
-from distutils.core import setup
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 setup(name='SpimRender',
-      version='0.1',
-      description='renders spim data in 3D',
-      url='git@bitbucket.org:mweigert/spimagine.git',
-      author='Martin Weigert',
-      author_email='mweigert@mpi-cbg.de',
-      license='MIT',
-      packages=['spimagine'],
-      package_data={"spimagine":['volume_render.cl']},
-      data_files=[("images","images/icon_start.png")],
-      scripts=['spimagine/spim_render.py',"spimagine/spimagine.py"],
-      install_requires=[
-          'numpy', 'scipy','Pillow',"PyOpenGL","pyopencl"
-          ,"SpimUtils","PyOCL"
-          ],
+    version='0.1',
+    description='renders spim data in 3D',
+    url='http://mweigert@bitbucket.org/mweigert/spimagine',
+    author='Martin Weigert',
+    author_email='mweigert@mpi-cbg.de',
+    license='MIT',
+    packages=['spimagine'],
+    install_requires=[
+        'numpy', 'scipy','Pillow',"PyOpenGL","pyopencl"
+        ,"SpimUtils","PyOCL"
+    ],
 
-      dependency_links=[
-              "http://mweigert@bitbucket.org/mweigert/spimutils",
-              "http://mweigert@bitbucket.org/mweigert/pyocl"]
-      )
+    package_data={"spimagine":['kernels/*','images/*']},
+    # entry_points = {
+    # 'console_scripts': [
+    # 'mypkg_starter = mypkg:main',
+    # ] },
+
+
+)
