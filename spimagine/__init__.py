e4de442d7cee432e390cb2c93c68bfabb81989f8
Martin Weigert <mweigert@mpi-cbg.de>
2014-10-23 21:01:10 +0200

    minor
    Former-commit-id: 5094d3b27f5224535c904e280f7f2099eae32918

--- spimagine/__init__.py
+++ spimagine/__init__.py
@@ -1,4 +1,41 @@
+import os
+__CONFIGFILE__ = os.path.expanduser("~/.spimagine")
+
+
 global __OPENCLDEVICE__
+__OPENCLDEVICE__ = None
+
+
+import ConfigParser, StringIO
+
+class MyConfigParser(ConfigParser.ConfigParser):
+    def __init__(self,fName = None):
+        ConfigParser.ConfigParser.__init__(self)
+        self.dummySection = "DUMMY"
+        if fName:
+            self.read(fName)
+
+
+    def read(self, fName):
+        try:
+            text = open(fName).read()
+        except IOError:
+            raise IOError()
+        else:
+            file = StringIO.StringIO("[%s]\n%s"%(self.dummySection,text))
+            self.readfp(file, fName)
+
+    def get(self,varStr):
+        return ConfigParser.ConfigParser.get(self,self.dummySection,varStr)
+
+
+if not __OPENCLDEVICE__:
+    try:
+        c = MyConfigParser(__CONFIGFILE__)
+        __OPENCLDEVICE__ = int(c.get("OPENCLDEVICE"))
+    except:
+        __OPENCLDEVICE__ = 0
+
 
 def setOpenCLDevice(num):
     global __OPENCLDEVICE__
@@ -6,10 +43,6 @@ def setOpenCLDevice(num):
 
 
 
-
-setOpenCLDevice(0)
-
-    
 import logging
 logging.basicConfig(format='%(levelname)s:%(name)s | %(message)s')
 logger = logging.getLogger(__name__)
@@ -17,7 +50,6 @@ logger.setLevel(logging.INFO)
 # logger.setLevel(logging.DEBUG)
 
 
-
-from spimagine.volume_render import *
+# from spimagine.volume_render import VolumeRenderer
 
 from spimagine.volshow import volshow, volfig
---
+++ spimagine/config_reader.py
@@ -0,0 +1,31 @@
+import ConfigParser, StringIO
+
+class MyConfigParser(ConfigParser.ConfigParser):
+    def __init__(self,fName = None):
+        ConfigParser.ConfigParser.__init__(self)
+        self.dummySection = "DUMMY"
+        if fName:
+            self.read(fName)
+
+
+    def read(self, fName):
+        try:
+            text = open(fName).read()
+        except IOError:
+            raise IOError()
+        else:
+            file = StringIO.StringIO("[%s]\n%s"%(self.dummySection,text))
+            self.readfp(file, fName)
+
+    def get(self,varStr):
+        return ConfigParser.ConfigParser.get(self,self.dummySection,varStr)
+
+
+
+if __name__ == '__main__':
+
+    import os
+    fName = os.path.expanduser("~/.spimagine")
+
+
+    c = MyConfigParser(fName)
