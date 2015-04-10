17740b70f147ab44e0432e4640e15015a315641d
Martin Weigert <mweigert@mpi-cbg.de>
2015-04-10 13:58:03 +0200

    version incremented for develop
    Former-commit-id: 6b0732607a05b3a000a15534a4f4318866f1e0b6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 here = os.path.abspath(os.path.dirname(__file__))
 
 setup(name='spimagine',
-    version='0.1',
+    version='0.2',
     description='renders spim data in 3D',
     url='http://mweigert@bitbucket.org/mweigert/spimagine',
     author='Martin Weigert',
