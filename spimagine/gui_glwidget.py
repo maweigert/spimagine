314f7a6fcc3c4635959a43bef7ecdecec1cbcc02
Martin Weigert <mweigert@mpi-cbg.de>
2015-08-19 10:43:16 +0200

    clean up (np * import, remove libtiff dep)

--- spimagine/gui_glwidget.py
+++ spimagine/gui_glwidget.py
@@ -62,16 +62,15 @@ from spimagine.data_model import DataModel
 
 from spimagine.transform_model import TransformModel
 
-from numpy import *
 import numpy as np
-
 from spimagine.gui_utils import *
 
 
 # on windows numpy.linalg.inv crashes without notice, so we have to import scipy.linalg
 if os.name == "nt":
     from scipy import linalg
-
+else:
+    from numpy import linalg
 
 import time
 from spimagine.quaternion import Quaternion
@@ -235,8 +234,8 @@ void main()
 """
 
 def _next_golden(n):
-    res = round((sqrt(5)-1.)/2.*n)
-    return int(round((sqrt(5)-1.)/2.*n))
+    res = round((np.sqrt(5)-1.)/2.*n)
+    return int(round((np.sqrt(5)-1.)/2.*n))
 
 
 
@@ -271,10 +270,10 @@ class GLWidget(QtOpenGL.QGLWidget):
         self.renderer.set_projection(mat4_perspective(60,1.,.1,100))
         # self.renderer.set_projection(projMatOrtho(-2,2,-2,2,-10,10))
 
-        self.output = zeros([self.renderer.height,self.renderer.width],dtype = np.float32)
-        self.output_alpha = zeros([self.renderer.height,self.renderer.width],dtype = np.float32)
+        self.output = np.zeros([self.renderer.height,self.renderer.width],dtype = np.float32)
+        self.output_alpha = np.zeros([self.renderer.height,self.renderer.width],dtype = np.float32)
 
-        self.sliceOutput = zeros((100,100),dtype = np.float32)
+        self.sliceOutput = np.zeros((100,100),dtype = np.float32)
 
         self.setTransform(TransformModel())
 
@@ -445,8 +444,8 @@ class GLWidget(QtOpenGL.QGLWidget):
     def dataModelChanged(self):
         if self.dataModel:
             self.renderer.set_data(self.dataModel[0], autoConvert = True)
-            self.transform.reset(minVal = amin(self.dataModel[0]),
-                                 maxVal = amax(self.dataModel[0]),
+            self.transform.reset(minVal = np.amin(self.dataModel[0]),
+                                 maxVal = np.amax(self.dataModel[0]),
                                  stackUnits= self.dataModel.stackUnits())
 
             self.refresh()
@@ -460,8 +459,8 @@ class GLWidget(QtOpenGL.QGLWidget):
 
     def dataSourceChanged(self):
         self.renderer.set_data(self.dataModel[0],autoConvert = True)
-        self.transform.reset(minVal = amin(self.dataModel[0]),
-                             maxVal = amax(self.dataModel[0]),
+        self.transform.reset(minVal = np.amin(self.dataModel[0]),
+                             maxVal = np.amax(self.dataModel[0]),
                              stackUnits= self.dataModel.stackUnits())
         self.refresh()
 
@@ -521,7 +520,7 @@ class GLWidget(QtOpenGL.QGLWidget):
 
             proj = self.transform.getProjection()
 
-            self.finalMat = dot(proj,modelView)
+            self.finalMat = np.dot(proj,modelView)
 
 
             self.textureAlpha = fillTexture2d(self.output_alpha,self.textureAlpha)
@@ -707,7 +706,7 @@ class GLWidget(QtOpenGL.QGLWidget):
     def wheelEvent(self, event):
         """ self.transform.zoom should be within [1,2]"""
         newZoom = self.transform.zoom * 1.2**(event.delta()/1400.)
-        newZoom = clip(newZoom,.4,3)
+        newZoom = np.clip(newZoom,.4,3)
         self.transform.setZoom(newZoom)
 
         logger.debug("newZoom: %s",newZoom)
@@ -716,13 +715,13 @@ class GLWidget(QtOpenGL.QGLWidget):
 
     def posToVec3(self,x,y, r0 = .8, isRot = True ):
         x, y = 2.*x/self.width-1.,1.-2.*y/self.width
-        r = sqrt(x*x+y*y)
+        r = np.sqrt(x*x+y*y)
         if r>r0-1.e-7:
             x,y = 1.*x*r0/r, 1.*y*r0/r
-        z = sqrt(max(0,r0**2-x*x-y*y))
+        z = np.sqrt(max(0,r0**2-x*x-y*y))
         if isRot:
-            M = linalg.inv(self.transform.quatRot.toRotation3())
-            x,y,z = dot(M,[x,y,z])
+            M = np.linalg.inv(self.transform.quatRot.toRotation3())
+            x,y,z = np.dot(M,[x,y,z])
 
         return x,y,z
 
@@ -758,11 +757,11 @@ class GLWidget(QtOpenGL.QGLWidget):
         if event.buttons() == QtCore.Qt.LeftButton:
 
             x1,y1,z1 = self.posToVec3(event.x(),event.y())
-            n = cross(array([self._x0,self._y0,self._z0]),array([x1,y1,z1]))
+            n = np.cross(np.array([self._x0,self._y0,self._z0]),np.array([x1,y1,z1]))
             nnorm = linalg.norm(n)
-            if abs(nnorm)>=1.:
-                nnorm *= 1./abs(nnorm)
-            w = arcsin(nnorm)
+            if np.abs(nnorm)>=1.:
+                nnorm *= 1./np.abs(nnorm)
+            w = np.arcsin(nnorm)
             n *= 1./(nnorm+1.e-10)
             q = Quaternion(np.cos(.5*w),*(np.sin(.5*w)*n))
             self.transform.setQuaternion(self.transform.quatRot*q)
@@ -771,7 +770,7 @@ class GLWidget(QtOpenGL.QGLWidget):
         if event.buttons() == QtCore.Qt.RightButton:
             x, y = self.posToVec2(event.x(),event.y())
 
-            dx, dy, foo = dot(self._invRotM,[x-self._x0, y-self._y0,0])
+            dx, dy, foo = np.dot(self._invRotM,[x-self._x0, y-self._y0,0])
 
             self.transform.addTranslate(dx,dy,foo)
             self._x0,self._y0 = x,y
@@ -799,10 +798,10 @@ def test_sphere():
 
     Ns = 5
     r = .6
-    phi = linspace(0,2*pi,Ns+1)[:-1]
-    d = zeros_like(X)
+    phi = np.linspace(0,2*pi,Ns+1)[:-1]
+    d = np.zeros_like(X)
     for p in phi:
-        d += 100.*exp(-10*(Z**2+(Y-r*sin(p))**2+(X-r*cos(p))**2))
+        d += 100.*np.exp(-10*(Z**2+(Y-r*np.sin(p))**2+(X-r*np.cos(p))**2))
 
 
     win.setModel(DataModel(NumpyData(d)))
@@ -819,26 +818,12 @@ def test_sphere():
 def test_demo():
 
     from data_model import DataModel, DemoData, SpimData, TiffData, NumpyData
-    import imgtools
     
     app = QtGui.QApplication(sys.argv)
 
     win = GLWidget(size=QtCore.QSize(800,800))
 
-    N = 256
-
-    d = imgtools.ZYX(N)[0]
-    
-    d = 100*exp(-100*d**2)
-    win.NSubrenderSteps = 1
-    
-    win.setModel(DataModel(NumpyData(d)))
-    
-    # win.setModel(DataModel(DemoData()))
-
-
-    # win.transform.setValueScale(0,10000.)
-    # win.transform.setBox(False)
+    win.setModel(DataModel(DemoData()))
 
     win.show()
 
