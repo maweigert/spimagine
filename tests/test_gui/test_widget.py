bcb986bbba4d3023253b62d6d97d61d24243484a
Martin Weigert <mweigert@mpi-cbg.de>
2016-06-15 12:04:57 +0200

    test,images

--- tests/test_gui/test_widget.py
+++ tests/test_gui/test_widget.py
@@ -2,12 +2,15 @@ __author__ = 'mweigert'
 
 import sys
 
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
 from spimagine.gui.mainwidget import MainWidget
 from spimagine.models.data_model import DemoData, DataModel
 
-if __name__ == '__main__':
+
+def f():
+    print "hallao"
+def test_widget():
     app = QtGui.QApplication(sys.argv)
 
 
@@ -18,4 +21,11 @@ if __name__ == '__main__':
     win.show()
     win.raise_()
 
-    sys.exit(app.exec_())
+    QtCore.QTimer.singleShot(1000,win.closeMe)
+
+    app.exec_()
+
+
+
+if __name__ == '__main__':
+    test_widget()
