7268394d30cd6fe5f7635a2b124341bb34d29fc3
Martin Weigert <mweigert@mpi-cbg.de>
2017-01-11 22:18:58 +0100

    setup updated

--- setup.py
+++ setup.py
@@ -1,45 +1,58 @@
 import os
 from setuptools import setup, find_packages
 
-
-
-exec(open('spimagine/version.py').read())
-
+exec (open('spimagine/version.py').read())
 
 setup(name='spimagine',
-    version=__version__,
-    description='OpenCL volume rendering in 3D/4D',
-    url='https://github.com/maweigert/spimagine',
-    author='Martin Weigert',
-    author_email='mweigert@mpi-cbg.de',
-    license='BSD 3-Clause License',
-    packages=find_packages(),
-    install_requires=[
-        'numpy',
-        'scipy',
-        'Pillow>=3.3',
-        "PyOpenGL",
-        "pyopencl",
+      version=__version__,
+      description='OpenCL volume rendering in 3D/4D',
+      url='https://github.com/maweigert/spimagine',
+      author='Martin Weigert',
+      author_email='mweigert@mpi-cbg.de',
+      license='BSD 3-Clause License',
+      packages=find_packages(),
 
-        "sortedcontainers"
-    ],
-    extras_require={
-    ':python_version>="3.2"': ["pyqt5",
-                               ],
-    },
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Science/Research',
+          'Topic :: Software Development :: Scientific/Engineering',
+          'License :: OSI Approved :: BSD License',
 
-    package_data={"spimagine":['volumerender/kernels/*',
-                               'gui/shaders/*',
-                               'gui/images/*',
-                               'colormaps/*',
-                               'data/*',
-                               'lib/*']},
-      entry_points = {
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+      ],
+
+      install_requires=[
+          'numpy',
+          'scipy',
+          'Pillow>=3.3',
+          "PyOpenGL",
+          "pyopencl",
+          "sortedcontainers"
+      ],
+      extras_require={
+          ':python_version<"3.0"': ["scikit-tensor",
+                                    "ConfigParser",
+                                    ],
+          ':python_version>="3.2"': ["configparser",
+                                    "pyqt5"
+                                     ],
+      },
+
+
+      package_data={"spimagine": ['volumerender/kernels/*',
+                                  'gui/shaders/*',
+                                  'gui/images/*',
+                                  'colormaps/*',
+                                  'data/*',
+                                  'lib/*']},
+      entry_points={
           'console_scripts': [
               'spimagine_render = spimagine.bin.spim_render:main'
           ],
-    'gui_scripts': [
-        'spimagine = spimagine.bin.spimagine_gui:main'
-    ]
-        }
-)
+          'gui_scripts': [
+              'spimagine = spimagine.bin.spimagine_gui:main'
+          ]
+      }
+      )
--- spimagine/version.py
+++ spimagine/version.py
@@ -6,4 +6,4 @@ mweigert@mpi-cbg.de
 """
 
 
-__version__ = "0.2.0a"
+__version__ = "0.2.0"
