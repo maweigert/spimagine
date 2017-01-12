9828e6fb7e0bddf51504ce81fd925b1a7eed79eb
Martin Weigert <mweigert@mpi-cbg.de>
2017-01-12 05:39:41 +0100

    pypi script

--- build_pypi.sh
+++ build_pypi.sh
@@ -1,7 +1,14 @@
 rm dist/*
-python setup.py sdist bdist_wheel
+
+python2 setup.py bdist_wheel
+python3 setup.py bdist_wheel
+
+
 # python setup.py register -r pypitest
 # twine upload -r pypitest dist/gputools*
+# python setup.py register
 
-python setup.py register 
-twine upload dist/spimagine*
+# twine upload dist/*whl
+
+
+
