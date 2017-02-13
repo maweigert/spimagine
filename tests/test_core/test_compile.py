0b746ba3125b64003450291b2a09a7bc95da4ad9
Martin Weigert <mweigert@mpi-cbg.de>
2017-02-13 20:38:04 +0100

    added test for compile errors

---
+++ tests/test_core/test_compile.py
@@ -0,0 +1,8 @@
+"""
+
+mweigert@mpi-cbg.de
+"""
+from __future__ import print_function, unicode_literals, absolute_import, division
+import os
+os.environ["PYOPENCL_COMPILER_OUTPUT"]="1"
+import spimagine
